aace65704246fd5888e372de28f8c89685450f6b
ohrtann19 <anna_ohrt@brown.edu>
2022-11-30 16:40:58 -0500

    error page done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,74 @@
 import "./App.css";
 import { useState } from "react";
-import bakeryData from "./assets/bakery-data.json";
+// import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
-bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
-});
+// bakeryData.forEach((item) => {
+//   item.image = process.env.PUBLIC_URL + "/" + item.image;
+// });
 /* ############################################################## */
-
+// inspiration taken from github's 404 page
 function App() {
+  const [isActive, setIsActive] = useState(false);
+  
+  const handleClick = () => {
+    // 👇️ toggle
+    setIsActive(!isActive);
+
+    // 👇️ or set to true
+    // setIsActive(true);
+  };
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
   return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    // <div className="App">
+    //   <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+    //   {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+    //     <p>Bakery Item {index}</p> // replace with BakeryItem component
+    //   ))}
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
+    //   <div>
+    //     <h2>Cart</h2>
+    //     {/* TODO: render a list of items in the cart */}
+    //   </div>
+    // </div>
+
+    <div class="container" style={{
+      backgroundColor: isActive ? 'black' : 'white'
+    }}>
+
+      <h1>404</h1>
+      <p><strong>Exit does not exist</strong></p>
+
+      <p>
+        The site configured at this address does not
+        contain the requested exit.
+      </p>
+
+      <p>
+        If this is your only hope, keep searching.
+      </p>
+
+      {/* <p>
+        <a href="https://help.github.com/pages/">Read the full documentation</a>
+        for more information about using <strong>GitHub Pages</strong>.
+      </p>
+
+      <div id="suggestions">
+        <a href="https://githubstatus.com">GitHub Status</a> &mdash;
+        <a href="https://twitter.com/githubstatus">@githubstatus</a>
+      </div> */}
+      <button style={{visibility: isActive ? "hidden" : "visible"}} onClick={handleClick}>shut down</button>
+      <a style={{visibility: isActive ? "visible" : "hidden"}} href="https://ohrtann19.github.io/epicfail/">Perform</a>
     </div>
+    
   );
 }
 
 export default App;
+
+// make sure that the filename case matches the URL.<br></br>
+// For root URLs (like <code>http://example.com/</code>) you must provide an
+// <code>index.html</code> file.
