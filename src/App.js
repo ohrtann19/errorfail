f5a6d92f24985029edb8bafb689bfcaf6f67abe5
ohrtann19 <anna_ohrt@brown.edu>
2023-04-10 11:01:08 -0400

    restore error page?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
-import { useState } from "react";
+// import "./App.css";
+// import { useState } from "react";
 // import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
@@ -17,7 +17,7 @@ function App() {
     setIsActive(!isActive);
 
     // 👇️ or set to true
-    // setIsActive(true);
+    setIsActive(true);
   };
 
   const handleLinkClick = () => {
@@ -39,7 +39,7 @@ function App() {
     //     {/* TODO: render a list of items in the cart */}
     //   </div>
     // </div>
-
+    <div class="App">
     <div class="container" style={{
       backgroundColor: isActive ? 'black' : 'white'
     }}>
@@ -56,7 +56,7 @@ function App() {
         If this is your only hope, keep searching.
       </p>
 
-      {/* <p>
+      <p>
         <a href="https://help.github.com/pages/">Read the full documentation</a>
         for more information about using <strong>GitHub Pages</strong>.
       </p>
@@ -64,16 +64,48 @@ function App() {
       <div id="suggestions">
         <a href="https://githubstatus.com">GitHub Status</a> &mdash;
         <a href="https://twitter.com/githubstatus">@githubstatus</a>
-      </div> */}
+      </div>
       <button style={{visibility: isActive ? "hidden" : "visible"}} onClick={handleClick}>shut down</button>
       <a onFocus={handleLinkClick} style={{visibility: isActive ? "visible" : "hidden", color: linkActive ? "red" : "black"}} href="https://ohrtann19.github.io/epicfail/">Exit</a>
     </div>
-    
+    </div>
   );
 }
 
 export default App;
 
-// make sure that the filename case matches the URL.<br></br>
-// For root URLs (like <code>http://example.com/</code>) you must provide an
-// <code>index.html</code> file.
+// // make sure that the filename case matches the URL.<br></br>
+// // For root URLs (like <code>http://example.com/</code>) you must provide an
+// // <code>index.html</code> file.
+
+// import logo from './logo.svg';
+// import whiteImg from './white.jpg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <p>test</p>
+//         <img src={whiteImg} className="white-img" alt="test"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//         <img src={whiteImg} className="white-img" alt="test2"/>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
